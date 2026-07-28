7b98a8143a5cdc1e1b5968f29c5efdef8dd97557
AtiqurRahmanLabib <devmail.labib@gmail.com>
2026-07-28 16:04:56 +0600

    solved problem 04

--- problem04.js
+++ problem04.js
@@ -1,1 +1,36 @@
 // Problem 04 ==> FizzBuzz (1 to N)
+
+// Without function
+const number = 15;
+
+// for (let i = 1; i <= number; i++) {
+//   console.log("i =>", i);
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// With function
+
+function checkFizzBuzzSystem(number) {
+  for (let i = 1; i <= number; i++) {
+    console.log("i =>", i);
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+checkFizzBuzzSystem(number)
